5e4a3e4f457b0b3fdda214b960e86ce988662b47
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-16 23:40:10 +0300

    Wildcard included in the route to navigate users from an incorrect route

--- MyStore/src/app/app-routing.module.ts
+++ MyStore/src/app/app-routing.module.ts
@@ -6,15 +6,15 @@ import { ConfirmationComponent } from './components/confirmation/confirmation.co
 import { CartComponent } from './components/cart/cart.component';
 
 const routes: Routes = [
-  {path: '', component: ProductListComponent},
-  {path: 'product', component: ProductItemDetailComponent},
-  {path: 'cart', component: CartComponent},
-  {path: 'confirmation', component: ConfirmationComponent}
+  { path: '', component: ProductListComponent },
+  { path: 'product', component: ProductItemDetailComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'confirmation', component: ConfirmationComponent },
+  { path: '**', redirectTo: '/' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
